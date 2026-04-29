9cdc050774b3e5c3f68ad8a2c7864afbf6df3bfa
Gaspard <gnzamura@hotmail.com>
2026-04-29 20:06:37 +0100

    Added concurrently package

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -10,7 +10,7 @@ app.get('/', (req:Request, res:Response)=> {
     res.send("Hello World!");
 });
 app.get('/api/hello', (req:Request, res:Response)=> {
-    res.json({message:"Hello World!"});
+    res.json({message:"Hello World! From Server!"});
 });
 
 app.listen(port, () => {
